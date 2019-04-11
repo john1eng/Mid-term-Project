33673c8fd9e2bdeb8994bca80adae59773341ab1
john1eng <john1eng@hotmail.com>
2019-04-11 12:43:23 -0400

    first commit

---
+++ js/fruit-data1.js
@@ -0,0 +1,98 @@
+var fruitData = {
+  Strawberry: {
+    name: 'Strawberry',
+    type: 'Berries',
+    good_source_of: 'Vitamin *C, A, K',
+    can_be_eatten: 'Raw and cook',
+    store: 'Best stored in fridge',
+    ripen: 'Will not ripen after harvesting'
+  },
+  Apple: {
+    name: 'Apple',
+    type: 'Apple',
+    good_source_of: 'Vitamin A, C, K, Folate',
+    can_be_eatten: 'Raw and cook',
+    store: 'Best stored in fridge',
+    ripen: 'Will not ripen after harvesting'
+  },
+  Avacado: {
+    name: 'Avacado',
+    type: 'Drupes and berries',
+    good_source_of: 'Vitamin *C, K, Folate',
+    can_be_eatten: 'Raw and cook',
+    store: 'Store unripe at room temperature, Best stored in fridge after ripen',
+    ripen: 'Will ripen at room temperature'
+  },
+  Banana: {
+    name: 'Banana',
+    type: 'Tropical and exotic',
+    good_source_of: 'Vitamin *C, B6, Folate',
+    can_be_eatten: 'Raw and cook',
+    store: 'Store in dry place (not in fridge)',
+    ripen: 'Will ripen at room temperature'
+  },
+  Blueberry: {
+    name: 'Blueberry',
+    type: 'Berries',
+    good_source_of: 'Vitamin *C, K, *Fibre',
+    can_be_eatten: '',
+    store: 'Best stored in fridge',
+    ripen: 'Will not ripen after harvesting'
+  },
+  Mango: {
+    name: 'Mango',
+    type: 'Tropical and exotic',
+    good_source_of: 'Vitamin *A,*C, B6, K',
+    can_be_eatten: 'Raw and cook',
+    store: 'Store unripe at room temperature',
+    ripen: 'Will ripen at room temperature'
+  },
+  Orange: {
+    name: 'Orange',
+    type: 'Citrus',
+    good_source_of: 'Vitamin *C, A, Folate',
+    can_be_eatten: 'Raw and cook',
+    store: 'Can be store in fridge and dry places',
+    ripen: 'Will not ripen after harvesting'
+  },
+  Pinapple: {
+    name: 'Pinapple',
+    type: 'Tropical and exotic',
+    good_source_of: 'Vitamin *C, A, B6',
+    can_be_eatten: 'Raw and cook',
+    store: 'Store unripe at room temperature/ Best stored in fridge after ripen',
+    ripen: 'Will ripen at room temperature'
+},
+  Plum: {
+    name: 'Plum',
+    type: 'Stone Fruit',
+    good_source_of: 'Vitamin A, C, K',
+    can_be_eatten: 'Raw and cook',
+    store: 'Store unripe at room temperature/ Best stored in fridge after ripen',
+    ripen: 'Will ripen at room temperature'
+},
+  Raspberries: {
+    name: 'Raspberries',
+    type: 'Berries',
+    good_source_of: 'Vitamin *C, A, K, Folate',
+    can_be_eatten: 'Raw and cook',
+    store: 'Best stored in fridge',
+    ripen: 'Will not ripen after harvesting'
+},
+Tomato: {
+  name: 'Tomato',
+  type: 'Berries',
+  good_source_of: 'Vitamin *C, *A, *niacin',
+  can_be_eatten: 'Raw and cook',
+  store: 'Store in dry place (not in fridge)',
+  ripen: 'Will not ripen after harvesting'
+},
+Watermelon: {
+  name: 'Watermelon',
+  type: 'Melon',
+  good_source_of: 'Vitamin C, lycopene, carotenoids ',
+  can_be_eatten: 'Raw and cook',
+  store: 'Best stored in fridge',
+  ripen: 'Will not ripen after harvesting'
+}
+};
